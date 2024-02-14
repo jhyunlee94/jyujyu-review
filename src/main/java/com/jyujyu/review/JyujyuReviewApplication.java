7baad9977518ae918c661aebdb7a409f97382571
jhyun <jonghyunlee@i-bricks.co.kr>
2024-02-15 08:20:18 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/jyujyu/review/JyujyuReviewApplication.java
@@ -0,0 +1,13 @@
+package com.jyujyu.review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JyujyuReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JyujyuReviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jyujyu/review/JyujyuReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jyujyu.review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JyujyuReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
