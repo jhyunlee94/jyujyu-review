69065105b36cbf9a6d665ddb4ac0c86850f09fdc
jhyun <jonghyunlee@i-bricks.co.kr>
2024-02-15 11:03:11 +0900

    feat : "Add Domain"
    Menu, Restaurant, Review 추가

---
+++ src/main/java/com/jyujyu/review/domain/Restaurant.java
@@ -0,0 +1,25 @@
+package com.jyujyu.review.domain;
+
+import java.time.ZonedDateTime;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class Restaurant {
+	private final Long id;
+	private final String name;
+	private final String address;
+	private final ZonedDateTime createAt;
+	private final ZonedDateTime updateAt;
+
+	@Builder
+	public Restaurant(Long id, String name, String address, ZonedDateTime createAt, ZonedDateTime updateAt) {
+		this.id = id;
+		this.name = name;
+		this.address = address;
+		this.createAt = createAt;
+		this.updateAt = updateAt;
+	}
+
+}
