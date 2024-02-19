8477ef39b9482b4bc4b8ed053091fde3ccd5c3ac
jhyun <jonghyunlee@i-bricks.co.kr>
2024-02-19 09:17:34 +0900

    test : "맛집 리스트 가져오기 API Test FakeRepository 이용"

--- src/test/java/com/jyujyu/review/service/RestaurantServiceTest.java
+++ src/test/java/com/jyujyu/review/service/RestaurantServiceTest.java
@@ -1,5 +1,6 @@
 package com.jyujyu.review.service;
 
+import java.time.ZonedDateTime;
 import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
@@ -8,20 +9,45 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import com.jyujyu.review.controller.port.RestaurantService;
 import com.jyujyu.review.controller.response.RestaurantResponse;
 import com.jyujyu.review.domain.Restaurant;
+import com.jyujyu.review.mock.FakeRestaurantRepository;
 import com.jyujyu.review.service.port.RestaurantRepository;
 
 public class RestaurantServiceTest {
 
-	private RestaurantServiceImpl restaurantService;
+	private RestaurantServiceImpl restaurantServiceImpl;
+	private RestaurantService restaurantService;
 	private RestaurantRepository restaurantRepository;
 
 	@BeforeEach
 	public void init() {
+		// 테스트에서 사용할 고정된 시간값
+		ZonedDateTime fixedTime = ZonedDateTime.parse("2024-02-15T07:42:56Z");
+
 		// Mock 객체 생성
 		restaurantRepository = Mockito.mock(RestaurantRepository.class);
-		restaurantService = new RestaurantServiceImpl(restaurantRepository);
+		restaurantServiceImpl = new RestaurantServiceImpl(restaurantRepository);
+		// 객체 형태로 테스트 구성
+		FakeRestaurantRepository fakeRestaurantRepository = new FakeRestaurantRepository();
+		this.restaurantService = RestaurantServiceImpl.builder()
+			.restaurantRepository(fakeRestaurantRepository)
+			.build();
+		fakeRestaurantRepository.save(Restaurant.builder()
+			.id(1L)
+			.name("이름1")
+			.address("주소1")
+			.createAt(fixedTime)
+			.updateAt(fixedTime)
+			.build());
+		fakeRestaurantRepository.save(Restaurant.builder()
+			.id(2L)
+			.name("이름2")
+			.address("주소2")
+			.createAt(fixedTime)
+			.updateAt(fixedTime)
+			.build());
 	}
 
 	@Test
@@ -45,7 +71,7 @@ public class RestaurantServiceTest {
 			.map((list) -> new RestaurantResponse(list.getId(), list.getName(), list.getAddress(), list.getCreateAt(),
 				list.getUpdateAt()))
 			.toList();
-		List<RestaurantResponse> responses = restaurantService.getRestaurants();
+		List<RestaurantResponse> responses = restaurantServiceImpl.getRestaurants();
 
 		// then
 		Assertions.assertIterableEquals(expectList, responses);
@@ -54,6 +80,29 @@ public class RestaurantServiceTest {
 	@Test
 	@DisplayName("맛집 리스트 가져오기 API")
 	public void getRestaurantsTest2() {
+		// given
+		// 테스트에서 사용할 고정된 시간값
+		ZonedDateTime fixedTime = ZonedDateTime.parse("2024-02-15T07:42:56Z");
 
+		List<RestaurantResponse> expectedList = List.of(
+			RestaurantResponse.builder()
+				.id(1L)
+				.name("이름1")
+				.address("주소1")
+				.createAt(fixedTime)
+				.updateAt(fixedTime)
+				.build(),
+			RestaurantResponse.builder()
+				.id(2L)
+				.name("이름2")
+				.address("주소2")
+				.createAt(fixedTime)
+				.updateAt(fixedTime)
+				.build()
+		);
+		// when
+		List<RestaurantResponse> actualList = restaurantService.getRestaurants();
+		// then
+		Assertions.assertIterableEquals(expectedList, actualList);
 	}
 }
